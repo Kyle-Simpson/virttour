8e7edff2706f425f1bb1b2e09b241d31c1cd0888
Kyle Simpson <kes97@uw.edu>
2018-09-18 16:04:54 -0700

    Manually source audio for now

--- src/HubStop.js
+++ src/HubStop.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Link } from 'react-router-dom';
+import firebase from 'firebase';
 
 
 export class HubStop extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            audioFile:"",
+            stopLocation:"",
+            stopName:""
+        }
+    }
+
+    componentWillMount() {
+        let dbRef = firebase.database().ref('1');
+        dbRef.on('value', (snapshot) => {
+            let data = snapshot.val();
+            this.setState({
+                audioFile: data.audioFile,
+                stopLocation : data.stopLocation,
+                stopName: data.stopName
+            })
+        })
+    }
+
     render() {
         return(
             <div>
-                <h1>Husky Union Building</h1>
+                <h1>{this.state.stopName}</h1>
                 <audio controls>
-                    <source src="https://firebasestorage.googleapis.com/v0/b/virttour-2018.appspot.com/o/6.-%20The%20Ecstasy%20(Jupiter).mp3?alt=media&token=89d6bc22-3ee0-4505-adc8-6c1b5fb74377"></source>
+                    <source src="https://firebasestorage.googleapis.com/v0/b/virttour-2018.appspot.com/o/1%20GW%20Statue%20-%20Intro%20v7_mixdown_FNL.mp3?alt=media&token=b3181829-8b66-485b-b8f1-6af6d97a5a0a"></source>
                 </audio>
                 <Router>
                     <Link to="/nextStop"><button className="btn btn-primary">Next Stop</button></Link>
