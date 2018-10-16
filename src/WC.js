da3de11f24b2bab26d1373b4127bdd3f491c916b
Kyle Simpson <kes97@uw.edu>
2018-10-16 09:46:32 -0700

    Finish audio captioning

--- src/WC.js
+++ src/WC.js
@@ -10,7 +10,7 @@ export class WC extends Component {
                 <audio controls controlsList="nodownload">
                     <source src="https://firebasestorage.googleapis.com/v0/b/virttour-2018.appspot.com/o/12%20West%20Campus%20v4_mixdown_FNL.mp3?alt=media&token=500dfe09-8213-4f70-80c6-ffc71d1b5b23"></source>
                 </audio><br></br>
-                <a href="" style={{'font-family':'BodyFont', 'color':'white'}}><u>Captions</u></a>
+                <a href="https://firebasestorage.googleapis.com/v0/b/virttour-2018.appspot.com/o/12%20West%20Campus%20v4_mixdown_FNL.mp3.txt?alt=media&token=eda1e310-1d93-4628-91a5-8f0543561475" style={{'font-family':'BodyFont', 'color':'white'}}><u>Captions</u></a>
                 <Router>
                     <div>
                         <Link to="/GW2toWC"><button className="btn btn-primary" style={{'font-family':'BodyFont'}}>Previous Stop</button></Link>
